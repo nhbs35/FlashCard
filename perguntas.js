3fd21666521574af240551950683789f2444e571
nhbs35 <00001111742364sp@al.educacao.sp.gov.br>
2024-11-06 07:23:51 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'geografia',
+    'O que é gentrificação?',
+    'Gentrificação é o processo de mudar o caráter de um bairro através do influxo de residentes e empresas mais abastados.sss'
+)
+
+criaCartao(
+    'biografia',
+    'o que é mitocondria',
+    'As mitocôndrias são organelas presentes em praticamente todas as células eucarióticas, incluindo as células humanas. Elas são conhecidas como a "central energética" da célula'
+)
+
+criaCartao(
+    'biologia',
+    'Normalmente, quantos litros de sangue uma pessoa tem?',
+    'entre 4 a 6 litros'
+)
+
+criaCartao(
+    'geografia',
+    'Em que continente se encontra o maior deserto do mundo?',
+    'continente Antartida'
+)
